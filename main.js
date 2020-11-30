ef753971bb5d7679116faf90d599ac84b5d16128
Francesco <francesco.savignano96@gmail.com>
2020-11-30 12:48:46 +0100

    header impostato dinamicamente con vue e layout css base impostato

--- main.js
+++ main.js
@@ -1,7 +1,57 @@
 var app = new Vue({
     el: '#root',
     data: {
+        header_boxes:[
+            {
+                type: "link",
+                icon_class: "icon-1 fas fa-home",
+                h1: "",
+                h3: "",
+                p: "",
+            },
+            {
+                type: "link",
+                icon_class: "icon-2 fab fa-pagelines",
+                h1: "",
+                h3: "Treatments",
+                p: "Face & Body",
+            },
+            {
+                type: "link",
+                icon_class: "icon-2 fas fa-male",
+                h1: "",
+                h3: "About",
+                p: "Our Team",
+            },
+            {
+                type: "logo",
+                h1_1: "Avada",
+                h1_2: "SPA",
 
+            },
+            {
+                type: "link",
+                icon_class: "icon-2 fas fa-heart",
+                h1: "",
+                h3: "Journal",
+                p: "Tips & Tricks",
+            },
+            {
+                type: "link",
+                icon_class: "icon-2 fas fa-bookmark",
+                h1: "",
+                h3: "Book now",
+                p: "Special offers",
+            },
+            {
+                type: "link",
+                icon_class: "icon-1 fas fa-plus",
+                h1: "",
+                h3: "",
+                p: "",
+            },
+
+        ],
     },
     methods: {
 

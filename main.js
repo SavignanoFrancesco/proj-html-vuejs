02c1a6d88179c36aecc4db63bb43f2e9aed46686
Francesco <francesco.savignano96@gmail.com>
2020-11-30 18:17:59 +0100

    aggiunta proprietà active(true o false)  agli oggetti dell'header, per segnalare quale voce è attiva

--- main.js
+++ main.js
@@ -7,18 +7,21 @@ var app = new Vue({
                 icon_class: "fas fa-home",
                 h3: "",
                 p: "",
+                active: true,
             },
             {
                 type: "link",
                 icon_class: "fab fa-pagelines",
                 h3: "Treatments",
                 p: "Face & Body",
+                active: false,
             },
             {
                 type: "link",
                 icon_class: "fas fa-male",
                 h3: "About",
                 p: "Our Team",
+                active: false,
             },
             {
                 type: "logo",
@@ -29,18 +32,21 @@ var app = new Vue({
                 icon_class: "fas fa-heart",
                 h3: "Journal",
                 p: "Tips & Tricks",
+                active: false,
             },
             {
                 type: "link",
                 icon_class: "fas fa-bookmark",
                 h3: "Book now",
                 p: "Special offers",
+                active: false,
             },
             {
                 type: "link",
                 icon_class: "fas fa-plus",
                 h3: "",
                 p: "",
+                active: false,
             },
 
         ],
@@ -68,7 +74,18 @@ var app = new Vue({
         ],
     },
     methods: {
+        //toggle per definire quale voce della navbar è attiva
+        colorActive(index){
+            for (var i = 0; i < this.header_boxes.length; i++) {
+                this.header_boxes[i].active = false;
+            }
+            if (this.header_boxes[index].active == false) {
+                this.header_boxes[index].active = true;
+            }else{
+                this.header_boxes[index].active = false;
+            }
 
+        }
     },
     mounted(){
 
